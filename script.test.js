e401236ffe9cb3d262aa162553b984b126ae03f1
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-06 16:29:28 +1100

    group tests using describe

--- script.test.js
+++ script.test.js
@@ -1,8 +1,29 @@
 const googleSearch = require("./script");
 
-dbMock = ["test.com", "what.com"];
-it("is google search test", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("test", dbMock)).toEqual(["test.com"]);
-  expect(googleSearch("what", dbMock)).toEqual(["what.com"]);
+dbMock = [
+  "test.com",
+  "what.com",
+  "cutie.com",
+  "math.com",
+  "test.co",
+  "math.co",
+  "maths.com",
+  "math.io",
+];
+
+describe("googleSearch", () => {
+  it("is google search test", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("test", dbMock)).toEqual(["test.com", "test.co"]);
+    expect(googleSearch("what", dbMock)).toEqual(["what.com"]);
+  });
+
+  it("work with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("returns no more than three results", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
