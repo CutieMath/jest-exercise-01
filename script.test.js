0a52b312faf58ab35029fe0613b2ef9a46889abd
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-06 16:09:22 +1100

    grouping tests

--- script.test.js
+++ script.test.js
@@ -3,8 +3,6 @@ const googleSearch = require("./script");
 dbMock = ["test.com", "what.com"];
 it("is google search test", () => {
   expect(googleSearch("testtest", dbMock)).toEqual([]);
-});
-
-it("is google search test", () => {
   expect(googleSearch("test", dbMock)).toEqual(["test.com"]);
+  expect(googleSearch("what", dbMock)).toEqual(["what.com"]);
 });
