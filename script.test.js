9547d4e0f4d7bb899a621bc2b57e8db4f74703bc
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-06 16:08:04 +1100

    add the second test case for different values

--- script.test.js
+++ script.test.js
@@ -1,6 +1,10 @@
 const googleSearch = require("./script");
 
 dbMock = ["test.com", "what.com"];
-it("Test 01", () => {
+it("is google search test", () => {
   expect(googleSearch("testtest", dbMock)).toEqual([]);
+});
+
+it("is google search test", () => {
+  expect(googleSearch("test", dbMock)).toEqual(["test.com"]);
 });
