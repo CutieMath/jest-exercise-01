6e3ec10116e285a955a8d6a4a314887252def527
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-11 16:11:14 +1100

    test for async functions

--- script2.js
+++ script2.js
@@ -7,7 +7,6 @@ const getPeoplePromise = (fetch) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       return {
         count: data.count,
         results: data.results,
@@ -18,11 +17,13 @@ const getPeoplePromise = (fetch) => {
 const getPeople = async (fetch) => {
   const getRequest = await fetch("https://swapi.dev/api/people");
   const data = await getRequest.json();
-  console.log(data);
   return {
     count: data.count,
     results: data.results,
   };
 };
 
-getPeople(fetch);
+module.exports = {
+  getPeople,
+  getPeoplePromise,
+};
---
+++ script2.test.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+const swapi = require("./script2");
+
+it("calls swapi to get people", (done) => {
+  expect.assertions(1); // make sure the code is running
+  swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+    done();
+  });
+});
+
+it("calls swapi to get people method two", () => {
+  expect.assertions(1); // make sure the code is running
+  return swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+  });
+});
+
+it("calls swapi to get people with a promise", (done) => {
+  expect.assertions(1);
+  swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+    done();
+  });
+});
